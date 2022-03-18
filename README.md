76fec86c0fb1df171d8e517b15ed738d01154bc8
Shivangam Soni <shivangamsoni99@gmail.com>
2022-03-18 12:02:55 +0530

    Front-End 50% Complete

--- README.md
+++
@@ -1,3 +0,0 @@
-# React Projects
-
-Some are Learning Tutorial Projects &amp; Some are My Personal Projects
--- expense-tracker/src/Components/Header/index.js
+++
@@ -1,9 +0,0 @@
-const Header = () => {
-  return (
-    <header>
-      <h2>Expense Tracker</h2>
-    </header>
-  );
-};
-
-export default Header;
